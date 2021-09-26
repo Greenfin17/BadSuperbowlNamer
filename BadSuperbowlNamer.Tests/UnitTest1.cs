f94c2155f7defb71d671973e808483adf65aded6
John Maple <jmaple@bellsouth.net>
2021-09-25 19:19:23 -0500

    adding all tests from notion for the main translate function

--- BadSuperbowlNamer.Tests/UnitTest1.cs
+++ BadSuperbowlNamer.Tests/UnitTest1.cs
@@ -70,10 +70,31 @@ namespace BadSuperbowlNamer.Tests
         }
 
         [Theory]
-        [InlineData(3549, "MMMDXLIX")]
         [InlineData(1, "I")]
         [InlineData(3, "III")]
         [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(9, "IX")]
+        [InlineData(12, "XII")]
+        [InlineData(14, "XIV")]
+        [InlineData(29, "XXIX")]
+        [InlineData(44, "XLIV")]
+        [InlineData(45, "XLV")]
+        [InlineData(68, "LXVIII")]
+        [InlineData(83, "LXXXIII")]
+        [InlineData(97, "XCVII")]
+        [InlineData(99, "XCIX")]
+        [InlineData(499, "CDXCIX")]
+        [InlineData(500, "D")]
+        [InlineData(501, "DI")]
+        [InlineData(649, "DCXLIX")]
+        [InlineData(798, "DCCXCVIII")]
+        [InlineData(891, "DCCCXCI")]
+        [InlineData(1000, "M")]
+        [InlineData(1004, "MIV")]
+        [InlineData(1006, "MVI")]
+        [InlineData(1023, "MXXIII")]
+        [InlineData(3549, "MMMDXLIX")]
         public void translate_to_roman_theory(int numberToTranslate, string expectedResult)
         {
             // Arrange
